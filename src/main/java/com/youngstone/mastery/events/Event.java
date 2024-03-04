4b9c60178344553357dd0329d498bf8142831612
youngstone89 <youngstone89@icloud.com>
2024-03-05 08:16:01 +0900

    daemon threads

---
+++ src/main/java/com/youngstone/mastery/events/Event.java
@@ -0,0 +1,13 @@
+package com.youngstone.mastery.events;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Event {
+    String event;
+    Date date;
+}
---
+++ src/test/java/com/youngstone/mastery/events/CleanerTaskTest.java
@@ -0,0 +1,28 @@
+package com.youngstone.mastery.events;
+
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import org.junit.jupiter.api.Test;
+
+public class CleanerTaskTest {
+
+    @Test
+    void testClean() {
+        Deque<Event> deque = new ConcurrentLinkedDeque<Event>();
+
+        WriterTask writer = new WriterTask(deque);
+        for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+            Thread thread = new Thread(writer);
+            thread.start();
+        }
+        CleanerTask cleaner = new CleanerTask(deque);
+        cleaner.start();
+        try {
+            cleaner.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
